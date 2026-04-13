31f06ad461d74d5c0373f496ac85ff05bb429e52
z16166 <z16166@users.noreply.github.com>
2026-04-13 19:21:41 +0800

    docs: add missing copyright headers to .rs files

--- src/bin/make_ico.rs
+++ src/bin/make_ico.rs
@@ -1,3 +1,19 @@
+// Simple Image Viewer - A high-performance, cross-platform image viewer
+// Copyright (C) 2024-2026 Simple Image Viewer Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 /// One-shot ICO generator. Run with: cargo run --bin make_ico
 fn main() {
     use std::path::Path;
