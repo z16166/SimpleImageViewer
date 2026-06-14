56d8a65eef680b8a800516818cd12860ab3e05ad
zhangbo <zhangbo@gmail.com>
2026-06-14 21:58:05 +0800

    chore: add full gplv3 file headers (#103)

--- libraries/libheif-sys/build.rs
+++ libraries/libheif-sys/build.rs
@@ -1,3 +1,19 @@
+// Simple Image Viewer - A high-performance, cross-platform image viewer
+// Copyright (C) 2024-2026 Simple Image Viewer Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 /// On Linux, Rust defaults to `static:+bundle`, which copies archive members into dependent rlibs.
 /// That defers C++ objects (e.g. from `libheif.a`) until after `libstdc++.a` is scanned and can force
 /// `libstdc++.so.6` into `DT_NEEDED`. `static:-bundle` keeps the `.a` for the final link only.
--- src/hdr/raw_demosaic_gpu.rs
+++ src/hdr/raw_demosaic_gpu.rs
@@ -11,6 +11,10 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+//
 
 //! GPU RAW demosaicing compute shader implementation.
 
